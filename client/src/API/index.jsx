f665eeb7c1e7f59925be6ea377b8f52dba0b58b6
Riaj Hasan Pranto <54685875+Coder-pranto@users.noreply.github.com>
2023-05-28 11:41:23 +0600

    constants for action type are added

--- client/src/API/index.jsx
+++ client/src/API/index.jsx
@@ -4,3 +4,6 @@ const url = "http://localhost:5000/stories";
 
 export const fetchStories = async() => axios.get(url);
 export const createStories = async(story) => axios.post(url, story);
+export const updateStories = async(id,story) => axios.patch(`${url}/${id}`, story);
+export const likeStories = async(id) => axios.patch(`${url}/${id}/likeStory`);
+export const deleteStories = async(id,story) => axios.delete(`${url}/${id}`);
